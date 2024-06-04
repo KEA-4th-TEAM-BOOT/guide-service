bc5901a5e79e01f1a773c2d3666624fae896f8f0
hanseul37 <hanseul37@daum.net>
2024-06-04 09:32:09 +0900

    [FIX] cors main.py

--- main.py
+++ main.py
@@ -3,10 +3,23 @@ import uvicorn
 import config
 from fastapi import FastAPI
 from api import spelling, tag, writing
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 port = int(os.environ.get("GUIDE_PORT", 8003))
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,  # cross-origin request에서 cookie를 포함할 것인지 (default=False)
+    allow_methods=[""],     # cross-origin request에서 허용할 method들을 나타냄. (default=['GET']
+    allow_headers=["*"],     # cross-origin request에서 허용할 HTTP Header 목록
+)
 
 app.include_router(spelling.router)
 app.include_router(tag.router)
